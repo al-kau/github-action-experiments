5e73ab718556cd69fd430910026e3e57d7434a95
alexander <116556921+al-kau@users.noreply.github.com>
2023-04-28 01:57:42 +0300

    Update Program.cs

--- src/Console.App/Program.cs
+++ src/Console.App/Program.cs
@@ -1,8 +1,15 @@
-﻿using System.Reflection;
+using System.Reflection;
 
 Console.WriteLine("Hello, World!");
-Console.WriteLine($"Version: {Assembly.GetExecutingAssembly().GetName().Version}");
+Console.WriteLine($"CultureInfo: {Assembly.GetExecutingAssembly().GetName().CultureInfo}");		
 Console.WriteLine($"Name: {Assembly.GetExecutingAssembly().GetName().Name}");
-Console.WriteLine($"CultureInfo: {Assembly.GetExecutingAssembly().GetName().CultureInfo}");
 Console.WriteLine($"Title: {Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyTitleAttribute>()?.Title}");
+Console.WriteLine($"Product: {Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyProductAttribute>()?.Product}");
 Console.WriteLine($"Company: {Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyCompanyAttribute>()?.Company}");
+Console.WriteLine($"Description: {Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyDescriptionAttribute >()?.Description}");
+Console.WriteLine($"Copyright: {Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyCopyrightAttribute >()?.Copyright}");
+Console.WriteLine($"Trademark: {Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyTrademarkAttribute >()?.Trademark}");
+Console.WriteLine($"Version: {Assembly.GetExecutingAssembly().GetName().Version}");
+Console.WriteLine($"FileVersion: {Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyFileVersionAttribute>()?.Version}");
+Console.WriteLine($"InformationalVersion: {Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion}");
+Console.WriteLine($"Configuration: {Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyConfigurationAttribute>()?.Configuration}");
